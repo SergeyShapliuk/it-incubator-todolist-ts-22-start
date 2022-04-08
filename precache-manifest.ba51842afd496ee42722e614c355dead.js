336d48d7176e15cdf6287e8f7090789a2917b397
SergeyShapliuk <sergeshapluk@gmail.com>
2022-04-08 11:47:14 +0300

    Updates

---
+++ precache-manifest.ba51842afd496ee42722e614c355dead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd5d12a5dd1d9ac936647fc85fa3f30b",
+    "url": "/it-incubator-todolist-ts-start/index.html"
+  },
+  {
+    "revision": "754c0ec283c923f70ba7",
+    "url": "/it-incubator-todolist-ts-start/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/it-incubator-todolist-ts-start/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/it-incubator-todolist-ts-start/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "754c0ec283c923f70ba7",
+    "url": "/it-incubator-todolist-ts-start/static/js/main.3af24a51.chunk.js"
+  },
+  {
+    "revision": "ef0cb0af71552bc2022c",
+    "url": "/it-incubator-todolist-ts-start/static/js/runtime-main.fd6c3591.js"
+  }
+]);
